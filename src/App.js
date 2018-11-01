1595d38f63bca704744407ab6fa38f571f2e4213
JanMach97 <janmachacek97@gmail.com>
2018-11-01 23:46:53 +0000

    create static components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import EpisodesList from './components/EpisodesList';
+import Player from './components/Player';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Navbar />
+      <EpisodesList />
+      <Player />
       </div>
     );
   }
---
+++ src/components/EpisodeCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EpisodeCard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>EpisodeCard</h1>
+      </div>
+    );
+  }
+}
+
+export default EpisodeCard;
