78f4be76d4d97899582bdeb0566518f0ddd739bf
JanMach97 <janmachacek97@gmail.com>
2018-11-02 00:42:52 +0000

    fetch data from api

--- src/components/EpisodesList.jsx
+++ src/components/EpisodesList.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import map from 'lodash.map'
 
 class EpisodesList extends Component {
+  state = {
+    episodes: {}
+  }
   render() {
     return (
       <div>
-        <h1>List</h1>
+        {map(this.state.episodes, (episode, i) => {
+          return <div key={i}>
+            <h1>{episode.title}</h1>
+          </div>
+        })}
       </div>
     );
+  }
+
+  componentDidMount() {
+    fetch('https://api.spreaker.com/v2/shows/1433865/episodes')
+    .then(buffer => buffer.json())
+    .then(({ response: { items } }) => {
+      this.setState({
+        episodes: items
+      })
+    })
   }
 }
 
