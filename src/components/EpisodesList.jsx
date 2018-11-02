6f1eac0a24732f1d74dd919992e97aa94ecd5f5d
JanMach97 <janmachacek97@gmail.com>
2018-11-02 11:53:23 +0000

    move fetchin episodes to app

--- src/components/EpisodesList.jsx
+++ src/components/EpisodesList.jsx
@@ -8,24 +8,14 @@ class EpisodesList extends Component {
   render() {
     return (
       <div>
-        {map(this.state.episodes, (episode, i) => {
+        {map(this.props.episodes, (episode, i) => {
           return <div key={i}>
-            <img src={episode.image_url} alt="episode"/>
+            <img onClick={() => this.props.selectEpisode(episode)} src={episode.image_url} alt="episode"/>
             <h3>{episode.title}</h3>
           </div>
         })}
       </div>
     );
-  }
-
-  componentDidMount() {
-    fetch('https://api.spreaker.com/v2/shows/1433865/episodes')
-    .then(buffer => buffer.json())
-    .then(({ response: { items } }) => {
-      this.setState({
-        episodes: items
-      })
-    })
   }
 }
 
