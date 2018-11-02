3b908a4e2e96f3e1c7f0a0d70bc34c34e272668b
JanMach97 <janmachacek97@gmail.com>
2018-11-02 09:21:54 +0000

    add player-center div with toggle play-pause button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
 class Player extends Component {
+  state = {
+    playing: false
+  }
   render() {
     return (
-      <div>
-        <h1>Player</h1>
+      <div className=''>
+        <audio ref='audio' src="https://api.spreaker.com/v2/episodes/16105972/play"></audio>
+        <div className='player-center'>
+          <i className="fas fa-step-backward"></i>
+          <i onClick={this.togglePlay} className={this.state.playing ? "far fa-pause-circle" : "far fa-play-circle"}></i>
+          <i className="fas fa-step-forward"></i>
+        </div>
       </div>
-    );
+    )
+  }
+
+  togglePlay = (e) => {
+    if(this.state.playing) {
+      this.refs.audio.pause()
+      this.setState({playing: false})
+    } else {
+      this.refs.audio.play()
+      this.setState({playing: true})
+    }
   }
 }
 
