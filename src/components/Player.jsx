fc7c73f2e50e9501a9f2c08bdd6710b0a5a410bc
JanMach97 <janmachacek97@gmail.com>
2018-11-02 11:50:29 +0000

    move audio tag from component to app

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,24 +7,18 @@ class Player extends Component {
   render() {
     return (
       <div className=''>
-        <audio ref='audio' src="https://api.spreaker.com/v2/episodes/16105972/play"></audio>
         <div className='player-center'>
+          <h3>{this.props.episode.title}</h3>
           <i className="fas fa-step-backward"></i>
-          <i onClick={this.togglePlay} className={this.state.playing ? "far fa-pause-circle" : "far fa-play-circle"}></i>
+          <i onClick={this.togglePlay} className={this.props.playing ? "far fa-pause-circle" : "far fa-play-circle"}></i>
           <i className="fas fa-step-forward"></i>
         </div>
       </div>
     )
   }
 
-  togglePlay = (e) => {
-    if(this.state.playing) {
-      this.refs.audio.pause()
-      this.setState({playing: false})
-    } else {
-      this.refs.audio.play()
-      this.setState({playing: true})
-    }
+  togglePlay = () => {
+    this.props.playing ? this.props.pause() : this.props.play()
   }
 }
 
