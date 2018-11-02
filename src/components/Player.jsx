0e7319e4481ff61ef9c41604fbe038c2a80a0f92
JanMach97 <janmachacek97@gmail.com>
2018-11-02 14:43:11 +0000

    add progress bar

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,12 +6,18 @@ class Player extends Component {
   }
   render() {
     return (
-      <div className=''>
+      <div className='player'>
         <div className='player-center'>
-          <h3>{this.props.episode.title}</h3>
+          <p>{this.props.episode.title}</p>
+          <div className='player-controller'>
           <i className="fas fa-step-backward"></i>
           <i onClick={this.togglePlay} className={this.props.playing ? "far fa-pause-circle" : "far fa-play-circle"}></i>
           <i className="fas fa-step-forward"></i>
+          </div>
+          <div className='progress'>
+            <div className='progress_filled'>
+            </div>
+          </div>
         </div>
       </div>
     )
