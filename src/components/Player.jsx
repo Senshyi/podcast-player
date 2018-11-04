721ac7ef4b77e542a1a8b291caf545a7925e1c69
JanMach97 <janmachacek97@gmail.com>
2018-11-04 19:43:06 +0000

    add duration and current time to player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,6 +3,7 @@ import { setInterval } from 'timers';
 
 class Player extends Component {
   state = {
+    currentTime: '0:00',
     percent: ''
   }
   render() {
@@ -11,13 +12,19 @@ class Player extends Component {
         <div className='player-center'>
           <p>{this.props.episode.title}</p>
           <div className='player-controller'>
-          <i className="fas fa-step-backward"></i>
-          <i onClick={this.togglePlay} className={this.props.playing ? "far fa-pause-circle" : "far fa-play-circle"}></i>
-          <i className="fas fa-step-forward"></i>
+            <i className="fas fa-step-backward"></i>
+            <i onClick={this.togglePlay} className={this.props.playing ? "far fa-pause-circle" : "far fa-play-circle"}></i>
+            <i className="fas fa-step-forward"></i>
           </div>
-          <div className='progress'>
-            <div style={{ flexBasis: this.state.percent}} className='progress_filled'>
+          <div className='player-progress'>
+              <span className='currentTime'>{this.state.currentTime}</span>
+            <div className='progress'>
+              <div style={{ flexBasis: this.state.percent}} className='progress_filled'>
+              </div>
             </div>
+            <span className='duration'>
+              {Object.keys(this.props.episode).length ? this.formatTime(this.props.episode.duration / 1000) : '0:00'}
+            </span>
           </div>
         </div>
       </div>
@@ -36,9 +43,16 @@ class Player extends Component {
       const audioDurInSec = this.props.episode.duration / 1000
       const percent = (this.props.audio.currentTime / audioDurInSec) * 100
       this.setState({
-        percent: `${percent}%`
+        percent: `${percent}%`,
+        currentTime: this.formatTime(this.props.audio.currentTime)
       })
     }
+  }
+
+  formatTime = (seconds) => {
+    const sec = parseInt(seconds % 60);
+    const min = parseInt((seconds / 60) % 60);
+    return `${min}:${sec < 10 ? '0' + sec : sec}`
   }
 }
 
