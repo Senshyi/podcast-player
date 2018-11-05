8bce7f412db6cea6f3b1ee049ecbdf3b8fb561ce
JanMach97 <janmachacek97@gmail.com>
2018-11-05 00:51:11 +0000

    fetch single episode details

--- src/api.js
+++ src/api.js
@@ -2,3 +2,8 @@ export const fetchEpisodes = () => {
   return fetch('https://api.spreaker.com/v2/shows/1433865/episodes')
     .then(buffer => buffer.json())
 }
+
+export const fetchSingleEpisode = (episodeId) => {
+  return fetch(`https://api.spreaker.com/v2/episodes/${episodeId}`)
+    .then(buffer => buffer.json())
+}
