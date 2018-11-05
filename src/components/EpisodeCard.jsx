ab2f77bc390f30ca0d3c4f474d42335e06ff1063
JanMach97 <janmachacek97@gmail.com>
2018-11-05 11:39:02 +0000

    add info about episode

--- src/components/EpisodeCard.jsx
+++ src/components/EpisodeCard.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import * as api from '../api'
 
 class EpisodeCard extends Component {
+  state = {
+    episode: {}
+  }
   render() {
     return (
       <div>
-        <h1>EpisodeCard</h1>
+        <h2>{this.state.episode.title}</h2>
+        <img src={this.state.episode.image_url} alt=""/>
+        <div className='description'>
+          <p>{this.state.episode.description}</p>
+        </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    api.fetchSingleEpisode(this.props.match.params.episode_id)
+      .then(({response: {episode}}) => {
+        this.setState({
+          episode
+        })
+      })
   }
 }
 
