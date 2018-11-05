ddb74cb7216bfe758013216087b059c729c91e4b
JanMach97 <janmachacek97@gmail.com>
2018-11-05 16:08:25 +0000

    add indicator if episode is playing

--- src/components/EpisodesList.jsx
+++ src/components/EpisodesList.jsx
@@ -8,10 +8,13 @@ class EpisodesList extends Component {
   }
   render() {
     return (
-      <div>
+      <div className='episodes-list'>
         {map(this.props.episodes, (episode, i) => {
-          return <div key={i}>
-            <img onClick={() => this.props.selectEpisode(episode, i)} src={episode.image_url} alt="episode"/>
+          return <div className='episode' key={i}>
+            <div onClick={() => this.props.selectEpisode(episode, i)} className='thumbnail'>
+            <i onClick={this.togglePlay} className={this.props.playing && this.props.index === i ? "far fa-pause-circle" : "far fa-play-circle"}></i>
+            <img src={episode.image_url} alt="episode"/>
+          </div>
             <Link to={`/episodes/${episode.episode_id}`}>
               <h3>{episode.title}</h3>
             </Link>
