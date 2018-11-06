96b6691561b9db54ae28152edf343a2c7470b77d
JanMach97 <janmachacek97@gmail.com>
2018-11-06 15:24:29 +0000

    test component rendering

--- src/components/__tests__/EpisodeCard.test.jsx
+++ src/components/__tests__/EpisodeCard.test.jsx
@@ -1,12 +1,15 @@
-import * as React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
-import EpisodeCard from '../EpisodeCard'
+import * as React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+import EpisodeCard from "../EpisodeCard";
 
 configure({ adapter: new Adapter() });
 
-describe('EpisodeCard', () => {
-  it('renders without crashing', () => {
-    shallow(<EpisodeCard match={{ params: { episode_id: '16105972'}}}/>)
-  })
-})
+describe("EpisodeCard", () => {
+  it("renders without crashing", () => {
+    const component = shallow(
+      <EpisodeCard match={{ params: { episode_id: "16105972" } }} />
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
