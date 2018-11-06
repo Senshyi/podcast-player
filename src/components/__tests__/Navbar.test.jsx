e39cd652a4ab416f40b49b6240333d5bb0c5fa0b
JanMach97 <janmachacek97@gmail.com>
2018-11-06 17:06:09 +0000

    add link test

--- src/components/__tests__/Navbar.test.jsx
+++ src/components/__tests__/Navbar.test.jsx
@@ -1,12 +1,17 @@
-import * as React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
-import Navbar from '../Navbar'
+import * as React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+import Navbar from "../Navbar";
 
 configure({ adapter: new Adapter() });
 
-describe('Navbar', () => {
-  it('renders without crashing', () => {
-    shallow(<Navbar />)
-  })
-})
+describe("Navbar", () => {
+  it("renders without crashing", () => {
+    const component = shallow(<Navbar />);
+    expect(component).toMatchSnapshot();
+  });
+  it("logo links to main page", () => {
+    const component = shallow(<Navbar />);
+    expect(component.find("Link").prop("to")).toBe("/");
+  });
+});
