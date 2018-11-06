98e7b2127a57e0ce2844fbc44437474f7516685f
JanMach97 <janmachacek97@gmail.com>
2018-11-06 17:26:37 +0000

    add input test

--- src/components/__tests__/Navbar.test.jsx
+++ src/components/__tests__/Navbar.test.jsx
@@ -5,6 +5,8 @@ import Navbar from "../Navbar";
 
 configure({ adapter: new Adapter() });
 
+const clickFn = jest.fn();
+
 describe("Navbar", () => {
   it("renders without crashing", () => {
     const component = shallow(<Navbar />);
@@ -13,5 +15,14 @@ describe("Navbar", () => {
   it("logo links to main page", () => {
     const component = shallow(<Navbar />);
     expect(component.find("Link").prop("to")).toBe("/");
+  });
+  it("value of input change state", () => {
+    const component = shallow(<Navbar filter={clickFn} />);
+    component.setState({ input: "" });
+
+    component.find("input").simulate("change", {
+      target: { value: "hello" }
+    });
+    expect(component.state("input")).toEqual("hello");
   });
 });
