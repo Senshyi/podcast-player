bcf38d23a94dda3ea36e63f69eaa8277a4b7a72d
JanMach97 <janmachacek97@gmail.com>
2018-11-06 14:27:12 +0000

    change setuo

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,4 @@
-import * as enzyme from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-// tslint:disable-next-line:no-any
-enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
