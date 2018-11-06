e950062aaa7976f64bcb359f6e7954a49601f70b
JanMach97 <janmachacek97@gmail.com>
2018-11-06 17:27:58 +0000

    add next and previous icon test

--- src/components/__tests__/Player.test.jsx
+++ src/components/__tests__/Player.test.jsx
@@ -1,12 +1,27 @@
-import * as React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
-import Player from '../Player'
+import * as React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+import Player from "../Player";
 
 configure({ adapter: new Adapter() });
 
-describe('Player', () => {
-  it('renders without crashing', () => {
-    shallow(<Player  episode={{}}/>)
-  })
-})
+const clickFn = jest.fn();
+
+describe("Player", () => {
+  it("renders without crashing", () => {
+    const component = shallow(<Player episode={{}} />);
+    expect(component).toMatchSnapshot();
+  });
+  it("click on icon next trigger a function", () => {
+    const component = shallow(<Player episode={{}} next={clickFn} />);
+
+    component.find(".fa-step-forward").simulate("click");
+    expect(clickFn).toHaveBeenCalled();
+  });
+  it("click on icon previous trigger a function", () => {
+    const component = shallow(<Player episode={{}} previous={clickFn} />);
+
+    component.find(".fa-step-backward").simulate("click");
+    expect(clickFn).toHaveBeenCalled();
+  });
+});
