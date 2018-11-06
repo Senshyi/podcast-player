b9c4430f765016318931a2b3a805b496c68b9e59
JanMach97 <janmachacek97@gmail.com>
2018-11-06 15:14:51 +0000

    move all css into one file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./App.css";
 import Navbar from "./components/Navbar";
 import EpisodesList from "./components/EpisodesList";
 import Player from "./components/Player";
